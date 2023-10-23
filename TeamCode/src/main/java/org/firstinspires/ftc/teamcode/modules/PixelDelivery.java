b2ec7c54969d500318d36b414c4620e6e03dc7f2
Vladimir <v.akhramovich@lab244.ru>
2023-10-23 20:50:59 +0300

    добавил поворот коробки и ее переворот

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/PixelDelivery.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/PixelDelivery.java
@@ -11,6 +11,11 @@ public class PixelDelivery {
     public static double DOOR_CLOSED_POSITION = 0.01;
     public static double DOOR_FULL_OPEN_POSITION = 0.26;
     public static double DOOR_HALF_OPENED_POSITION = 0.228;
+    public static double BOX_ROTATION_TAKE_POSITION = 0.12345678;
+    public static double BOX_ROTATION_DROP_POSITION = 0.123456789;
+    public static double FLIP_TAKE_POSITION = 0.1090190910953159009;
+    public static double FLIP_DROP_POSITION = 0.0000012;
+
     private final Servo servoDoor;
     private final Servo boxRotation;
     private final Servo servoFlipLeft;
@@ -23,6 +28,7 @@ public class PixelDelivery {
         this.boxRotation = opMode.hardwareMap.servo.get("boxRotation");
         this.servoFlipLeft = opMode.hardwareMap.servo.get("servoFlipLeft");
         this.servoFlipRight = opMode.hardwareMap.servo.get("servoFlipRight");
+        this.servoFlipRight.setDirection(Servo.Direction.REVERSE);
     }
 
     public void fullOpenDoor() {
@@ -37,4 +43,26 @@ public class PixelDelivery {
         servoDoor.setPosition(DOOR_CLOSED_POSITION);
     }
 
+    public void boxTakePixel() {
+        boxRotation.setPosition(BOX_ROTATION_TAKE_POSITION);
+    }
+
+    public void boxDropPixel() {
+        boxRotation.setPosition(BOX_ROTATION_DROP_POSITION);
+    }
+
+    public void flipTakePixel() {
+        servoFlipLeft.setPosition(FLIP_TAKE_POSITION);
+        servoFlipRight.setPosition(FLIP_TAKE_POSITION);
+    }
+
+    public void flipDropPixel() {
+        servoFlipLeft.setPosition(FLIP_DROP_POSITION);
+        servoFlipRight.setPosition(FLIP_DROP_POSITION);
+    }
+
+    public double flipGetPosition() {
+        return servoFlipLeft.getPosition();
+
+    }
 }
