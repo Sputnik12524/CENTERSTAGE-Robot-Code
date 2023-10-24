fefa247f7695a064810b3c52e19d5d6bb266d28b
Yaroslav Drozdov <i.drozdov@lab244.ru>
2023-10-24 18:21:54 +0300

    Исправил ошибки в модуле Recognition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Recognition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Recognition.java
@@ -9,27 +9,25 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Recognition {
-    public final DistanceSensor ds;
-    public static boolean isrecognized;
-    telemetry.addData("range", String.format("%.01f cm", ds.getDistance(DistanceUnit.CM)));
-    telemetry.appDate;
-    public boolean isRecognized(){
-        if (ds.getDistance(DistanceUnit.CM) < 200)
-        { //распознал
-            telemetry.addData("range", String.format("%.01f cm", ds.getDistance(DistanceUnit.CM));
-            telemetry.addLine("Распознал");
-            telemetry.appDate;
-            isrecognized = TRUE;
-        }
-        else{
-            telemetry.addData("range", String.format("%.01f cm", ds.getDistance(DistanceUnit.CM));
-            telemetry.addLine("Не распознал");
-            telemetry.appDate;
-            isrecognized = False;
-        }
+    private DistanceSensor ds;
+    private final Telemetry tm;
+
+    public Recognition(LinearOpMode opMode) {
+        //telemetry.addData("range", String.format("%.01f cm", ds.getDistance(DistanceUnit.CM)));
+        // telemetry.appDate
+        HardwareMap hm = opMode.hardwareMap;
+        ds = hm.get(DistanceSensor.class, "ds");
+        tm = opMode.telemetry;
+    }
+    public boolean isRecognized () {
+        tm.addData("Distance", ds.getDistance(DistanceUnit.CM));
+        tm.update();
+        boolean isrecognized = ds.getDistance(DistanceUnit.CM) < 200;
         return isrecognized;
     }
 }
