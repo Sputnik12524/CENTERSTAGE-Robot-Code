b82f51ae591566120170b3132e5616a49e9d2961
Yaroslav Drozdov <i.drozdov@lab244.ru>
2023-10-24 19:05:54 +0300

    исправил недочеты

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Recognition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Recognition.java
@@ -1,33 +1,27 @@
 package org.firstinspires.ftc.teamcode.modules;
 
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Recognition {
+    public static final int rangeToElement = 200;
     private DistanceSensor ds;
     private final Telemetry tm;
 
     public Recognition(LinearOpMode opMode) {
-        //telemetry.addData("range", String.format("%.01f cm", ds.getDistance(DistanceUnit.CM)));
-        // telemetry.appDate
         HardwareMap hm = opMode.hardwareMap;
         ds = hm.get(DistanceSensor.class, "ds");
         tm = opMode.telemetry;
     }
-    public boolean isRecognized () {
+
+    public boolean isRecognized() {
         tm.addData("Distance", ds.getDistance(DistanceUnit.CM));
         tm.update();
-        boolean isrecognized = ds.getDistance(DistanceUnit.CM) < 200;
+        boolean isrecognized = ds.getDistance(DistanceUnit.CM) < rangeToElement;
         return isrecognized;
     }
 }
