bbe9e6ddb6c185071d76b2b19d612636765f6a98
Yaroslav Drozdov <i.drozdov@lab244.ru>
2023-10-27 19:39:41 +0300

    наметил 2 автонома для красного альянса

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/RedAutoF2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/RedAutoF2.java
@@ -2,15 +2,15 @@ package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.modules.Drivetrain;
 import org.firstinspires.ftc.teamcode.modules.Intake;
+import org.firstinspires.ftc.teamcode.modules.Recognition;
 
 @Config
-@Autonomous(group="Auto", name = "RedAuto")
-public class RedAuto extends LinearOpMode {
+@Autonomous(group="Auto", name = "RedAutoF2")
+public class RedAutoF2 extends LinearOpMode {
     // единожды выполняемые действия до запуска программы
     // здесь следует создавать переменные и константы для сценария
 
@@ -19,6 +19,7 @@ public class RedAuto extends LinearOpMode {
         // единожды выполняемые действия до инициализации
         Drivetrain dt = new Drivetrain(this);
         Intake it = new Intake(this);
+        Recognition rc = new Recognition(this);
 
         while(opModeInInit()){
             // единожды выполняемые действия во время инициализации
@@ -29,9 +30,30 @@ public class RedAuto extends LinearOpMode {
         waitForStart();
         // единожды выполняемые действия после запуска сценария
 
-
-        while(opModeIsActive()){
             // единожды выполняемые действия после запуска сценария
+            dt.driveEncoder(500,0.3);
+            if (rc.isRecognized() == true){ //элемент по центру
+                dt.driveEncoder(1700,0.3);
+            }
+            else {
+                dt.rotate(90);
+                dt.driveEncoder(200, 0.3);
+                dt.rotate(-90);
+                if (rc.isRecognized() == true){ //элемент справа
+                    dt.driveEncoder(1200,0.3);
+                }
+                else { //элемент слева
+                    dt.rotate(-90);
+                    dt.driveEncoder(700,0.3);
+                    dt.rotate(90);
+                    dt.driveEncoder(1200,0.3);
+                }
+
+            dt.driveEncoder(1750,-0.3); //выравниваемся у борта
+            dt.driveEncoder(100,0.3);
+            dt.rotate(90);
+            dt.driveEncoder(5000,0.5);
+
 
 
         }
