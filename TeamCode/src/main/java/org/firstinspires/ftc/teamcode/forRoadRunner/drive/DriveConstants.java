f51ab7fcdf2f762698560ce0b880acae905ac60c
Y <i.drozdov@lab244.ru>
2023-12-13 19:05:55 +0300

    промежуточные коэфиценты

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/forRoadRunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/forRoadRunner/drive/DriveConstants.java
@@ -35,8 +35,8 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-      getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(12, 7, 8,
+            12.9);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -91,7 +91,7 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
      
      */
-    public static double MAX_VEL = 52.56591367802781;
+    public static double MAX_VEL = 47;
     public static double MAX_ACCEL = 52.56591367802781;
     public static double MAX_ANG_VEL = Math.toRadians(287.59453420419385);
     public static double MAX_ANG_ACCEL = Math.toRadians(287.59453420419385);
