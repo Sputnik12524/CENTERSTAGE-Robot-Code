10a4a331fa12294a97ccad48f81aca132a313841
Y <i.drozdov@lab244.ru>
2024-01-09 18:27:09 +0300

    адд RecognitionPhone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/RecognitionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/RecognitionExample.java
@@ -1,23 +1,23 @@
-package org.firstinspires.ftc.teamcode.examples;
-
-
-import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.modules.recognition.Recognition;
-
-@TeleOp(name = "RecognitionExample")
-@Config
-public class RecognitionExample extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
-
-        Recognition rc = new Recognition(this);
-        while (!isStarted() && !isStopRequested()) {
-            telemetry.addData("Распознал? ", rc.isRecognized());
-            telemetry.update();
-        }
-
-    }
-}
+//package org.firstinspires.ftc.teamcode.examples;
+//
+//
+//import com.acmerobotics.dashboard.config.Config;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//import org.firstinspires.ftc.teamcode.modules.recognition.Recognition;
+//
+//@TeleOp(name = "RecognitionExample")
+//@Config
+//public class RecognitionExample extends LinearOpMode {
+//    @Override
+//    public void runOpMode() throws InterruptedException {
+//
+//        Recognition rc = new Recognition(this);
+//        while (!isStarted() && !isStopRequested()) {
+//            telemetry.addData("Распознал? ", rc.isRecognized());
+//            telemetry.update();
+//        }
+//
+//    }
+//}
